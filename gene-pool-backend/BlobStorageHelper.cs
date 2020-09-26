c0b793e458695e6135a29e02c7593107ecab2a2e
Justin <justin.c.bang@gmail.com>
2020-09-26 17:31:55 -0400

    Unknown server bug

--- gene-pool-backend/BlobStorageHelper.cs
+++ gene-pool-backend/BlobStorageHelper.cs
@@ -37,14 +37,10 @@ namespace gene_pool_backend {
         wavfile = Path.Combine(folder, $"{title}.wav");
         wavname = $"{title}.wav";
 
-        try {
-          File.Delete(mp4file);
-          File.Delete(wavfile);
+        File.Delete(mp4file);
+        File.Delete(wavfile);
 
-          Debug.WriteLine("I got here 1");
-        } catch {
-          return false;
-        }
+        Debug.WriteLine("I got here 1");
 
         FileHelper.SaveVideoToDisk(url, mp4file);
         if (!FileHelper.ToWavFormat(mp4file, wavfile)) {
@@ -59,31 +55,25 @@ namespace gene_pool_backend {
           containerClient = blobServiceClient.GetBlobContainerClient(containerName);
         }
 
-        try {
-          Debug.WriteLine("I got here 2");
+        Debug.WriteLine("I got here 2");
 
-          string fileName = wavname;
+        // Get a reference to a blob
+        BlobClient blobClient = containerClient.GetBlobClient(wavname);
 
-          // Get a reference to a blob
-          BlobClient blobClient = containerClient.GetBlobClient(fileName);
+        Debug.WriteLine("I got here 3");
 
-          Debug.WriteLine("I got here 3");
+        Debug.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
 
-          Debug.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+        using FileStream uploadFileStream = File.OpenRead(wavfile);
+        await blobClient.UploadAsync(uploadFileStream, true);
+        uploadFileStream.Close();
 
-          using FileStream uploadFileStream = File.OpenRead(wavfile);
-          await blobClient.UploadAsync(uploadFileStream, true);
-          uploadFileStream.Close();
+        Debug.WriteLine("I got here 4");
 
-          Debug.WriteLine("I got here 4");
+        File.Delete(mp4file);
+        File.Delete(wavfile);
 
-          File.Delete(mp4file);
-          File.Delete(wavfile);
-
-          return true;
-        } catch {
-          return false;
-        }
+        return true;
       } catch {
         return false;
       }
